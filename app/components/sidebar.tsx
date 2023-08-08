c395235b664cbe30e7173ba1b79b7a271dca8b08
KiritoAndYuuki <129043517+KiritoAndYuuki@users.noreply.github.com>
2023-08-08 15:15:30 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -118,10 +118,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          SAO-kitiro
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          思考简介ing[OPEN AI]
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
